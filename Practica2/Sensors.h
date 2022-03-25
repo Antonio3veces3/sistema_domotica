8d98fe2964690d5fbb9e60e04e7272c1ea75ab7c
Mezly Mondragon <mmondragon@ucol.mx>
2022-03-25 11:00:17 -0600

    Luminosidad y Temperatura recibidas

--- Practica2/Sensors.h
+++ Practica2/Sensors.h
@@ -4,7 +4,6 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-//Sensor de luminosidad LDR
 //Sensor de movimiento PIR
 //Actuador Buzzer Activo
 //Actuador Relay de Estado Solido 
@@ -15,18 +14,26 @@ class class_sensors {
 
   public:                                   //Variables Públicas
     float temperatura, humedad;            //Reciben la temperatura y humedad del sensor DHT11
+    //const int pinDHT11 = 4;                //Pin para el foco led
+    const int pinFoco = 12;                //Pin para el foco led
+    const int pinLDR = 0;                 // Pin analogico de entrada para el LDR
+    int valorLDR = 0;                    // Variable donde se almacena el valor del LDR
 
  public:                                  //Métodos Públicas
    float obtener_temperatura ( void );   //Método para obtener la temperatura del sensor DHT11
    float obtener_humedad ( void );       //Método para obtener la humedad del sensor DHT11
-   void beginDHT11 ( void );
+   void beginDHT11 ( void );            //Método para inicilizar el sensor DHT11
+   int obtener_luminosidad ( void );               //Método para obtener la luminisidad
 };
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PINES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 //Sensor de temperatura y humedad DHT11
 DHT dht( 4, DHT11 );
+
 //Sensor de luminosidad LDR
+
+
 //Sensor de movimiento PIR
 //Actuador Buzzer Activo
 //Actuador Relay de Estado Solido 
@@ -52,6 +59,14 @@ float class_sensors::obtener_humedad ( void ){
 }
 
 //Sensor de luminosidad LDR
+int class_sensors::obtener_luminosidad ( void ){
+  return valorLDR = analogRead(pinLDR);
+}
+
+
+
+
+
 //Sensor de movimiento PIR
 //Actuador Buzzer Activo
 //Actuador Relay de Estado Solido 
--- Practica2/Tasks.h
+++ Practica2/Tasks.h
@@ -4,19 +4,22 @@ class class_tasks{
     unsigned long timeHum = 0;
     unsigned long intervaloTemp = 5000;
     unsigned long intervaloHum = 7000;
+    unsigned long timeLdr = 0;
+    unsigned long intervaloLdr = 2000;
 
   public:
     float get_temperature( void );
     float get_humidity( void );
+    int get_brightness (void);
 };
 
 float class_tasks::get_temperature( void ){
   if((millis() - timeTemp) >= intervaloTemp ){
     timeTemp = millis();
-    float tempSensor = sensor.obtener_temperatura();
+    float temperature = sensor.obtener_temperatura();
     Serial.print("Temperatura: ");
-    Serial.println(tempSensor);
-    return tempSensor;
+    Serial.println(temperature);
+    return temperature;
   }else{
     return 0;
   }
@@ -25,11 +28,22 @@ float class_tasks::get_temperature( void ){
 float class_tasks::get_humidity( void ){
   if((millis() - timeHum) >= intervaloHum ){
     timeHum = millis();
-    float humSensor = sensor.obtener_humedad();
+    float humidity = sensor.obtener_humedad();
     Serial.print("Humedad: ");
-    Serial.println(humSensor);
-    return humSensor;
+    Serial.println(humidity);
+    return humidity;
   }else{
      return 0;
+  }
+}
+
+int class_tasks::get_brightness(void){
+  if((millis() - timeLdr) >= intervaloLdr){
+    timeLdr = millis();
+    int brightness = sensor.obtener_luminosidad();
+    Serial.println("Luminosidad: " + String(brightness));
+    return brightness;
+  }else{
+    return -1;
   }
 }
