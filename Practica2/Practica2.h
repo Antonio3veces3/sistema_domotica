9d66ddc13a047f5f5b4489fe2638e09c82c3bd75
Alfredo (Charibu) <apaz0@ucol.mx>
2022-05-11 19:37:16 -0500

    Variables, clases y librerias

--- Practica2/Practica2.h
+++ Practica2/Practica2.h
@@ -21,6 +21,10 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+//Librerias para MQTT
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~DEFINICIÓN DE PINES Y EXTENSIONES DE ARCHIVOS~~~~~~~~~~~~~~~~~~~~~~~~*/
 #define MICROSD_PIN 5
 #define BUZZER_PIN 26
@@ -31,12 +35,21 @@
 
 #define EXTENSION ".json"
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~VARIABLES PARA MQTT~~~~~~~~~~~~~~~~~~~~~~~~*/
+const char* HOTSPOT_WIFI = "tony";
+const char* HOTSPOT_PWD = "tonyholamundo";  
+const char* MQTT_SERVER = "test.mosquitto.org";
+#define MQTT_PORT 1883
+#define TOPIC "esp32-RALMT/data"
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~CONFIGURACION~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 RTC_DS1307 rtc;
 DHT dht(DHT11_PIN, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2); //Configura la LCD a 16x2
 File microSD_file;
+WiFiClient ESP32_WIFI;
+PubSubClient client ( ESP32_WIFI );
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTCIÓN DE CLASES~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -54,6 +67,9 @@ class_sensors sensor;          //Importa la clase de sensores
 
 #include "LCD.h"
 class_lcd lcd;
+
+#include "MQTT.h"
+class_MQTT mqtt;              //Importa la clase de MQTT
 
 #include "Tasks.h";
 class_tasks tasks;             //Importa la clase de tasks
