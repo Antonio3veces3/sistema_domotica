0a7e5e9bcda6e092d170ee5e5b3714d513505a50
zaholy16 <mmondragon@ucol.mx>
2022-05-14 10:30:31 -0500

    :art: Revisión archivo header

--- Practica2/Practica2.h
+++ Practica2/Practica2.h
@@ -1,68 +1,60 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~ARCHIVO HEADER~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTACIÓN DE CLASES~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-#include <ArduinoJson.h>
-
-#include <LiquidCrystal_I2C.h>
-
-#include <SPI.h>
-#include <SD.h>
-
-//Librerías para manejo del módulo RTC
-#include <Wire.h>
-#include "RTClib.h"
-
-//Librerías para manejo del Buzzer
-#include <Config.h>
-#include <EasyBuzzer.h> 
-
-//Librerías para sensor DHT11
-#include <DHT.h>
-#include <DHT_U.h>
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTACIÓN DE LIBRERIAS~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 //Librerias para MQTT
 #include <WiFi.h>
+  WiFiClient ESP32_WIFI;
+  
 #include <PubSubClient.h>
+  PubSubClient client ( ESP32_WIFI );
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~DEFINICIÓN DE PINES Y EXTENSIONES DE ARCHIVOS~~~~~~~~~~~~~~~~~~~~~~~~*/
-#define MICROSD_PIN 5
-#define BUZZER_PIN 26
-#define DHT11_PIN 32
+#include <LiquidCrystal_I2C.h>
+  LiquidCrystal_I2C lcd(0x27, 16, 2); //Configura la LCD a 16x2
+
 #define PIR_PIN 15
+#define BUZZER_PIN 26
 #define RELAY_PIN 25
 #define LDR_PIN 39
 
+#include <Config.h>
+#include <EasyBuzzer.h> 
+
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHT11_PIN 32
+  DHT dht(DHT11_PIN, DHT11);
+
+#include <Wire.h>
+#include <ArduinoJson.h>
+#define MICROSD_PIN 5
 #define EXTENSION ".json"
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~CONFIGURACION~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-RTC_DS1307 rtc;
-DHT dht(DHT11_PIN, DHT11);
-LiquidCrystal_I2C lcd(0x27, 16, 2); //Configura la LCD a 16x2
+#include <SPI.h>
+#include <SD.h>
+#include "RTClib.h"
+ RTC_DS1307 rtc;
 File microSD_file;
-WiFiClient ESP32_WIFI;
-PubSubClient client ( ESP32_WIFI );
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTCIÓN DE CLASES~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTACIÓN DE CLASES~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 #include "RTC.h"
-class_rtc RTC;
-
-#include "MicroSD.h"
-class_microSD microSD;
-
-#include "Actuators.h";
-class_actuators actuator;    //Importa la clase de actuadores
+ class_rtc RTC;
 
 #include "Sensors.h";
-class_sensors sensor;          //Importa la clase de sensores
+ class_sensors sensor;          //Importa la clase de sensores
 
-#include "LCD.h"
-class_lcd lcd;
+#include "Actuators.h";
+ class_actuators actuator;      //Importa la clase de actuadores
+
+#include "MicroSD.h"
+ class_microSD MSD;          //Importa la clase de la microSD
+
+#include "LCD.h"              
+ class_lcd LCD;                //Importa la clase de LCD
 
 #include "MQTT.h"
-class_MQTT mqtt;              //Importa la clase de MQTT
+ class_MQTT mqtt;              //Importa la clase de MQTT
 
 #include "Tasks.h";
-class_tasks tasks;             //Importa la clase de tasks
+ class_tasks tasks;             //Importa la clase de tasks
