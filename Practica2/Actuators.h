22c66fb38b3dc6217a851d9ef7edb42884e61c7d
zaholy16 <mmondragon@ucol.mx>
2022-05-14 10:25:59 -0500

    :art: Revisión código Actuators.h

--- Practica2/Actuators.h
+++ Practica2/Actuators.h
@@ -1,38 +1,38 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CLASES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 class class_actuators {
  public:   //Métodos Públicos  
-    void initBuzzer(void);           
-    void EncenderLED(void); 
-    void ApagarLED(void);  
-    void EncenderBuzzer(void);
-    void ApagarBuzzer(void);
+    void initBuzzer( void );           
+    void EncenderLED( void ); 
+    void ApagarLED( void );  
+    void EncenderBuzzer( void );
+    void ApagarBuzzer( void );
 };
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MÉTODOS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 //Inicializa el buzzer
-void class_actuators::initBuzzer(void){
+void class_actuators::initBuzzer( void ){
   EasyBuzzer.setPin(BUZZER_PIN); 
 }
 
 //Enciende el LED
-void class_actuators::EncenderLED(void){
+void class_actuators::EncenderLED( void ){
     digitalWrite(RELAY_PIN,LOW); //PIN RELAY
 }
 
 //Apaga el LED
-void class_actuators::ApagarLED(void){
+void class_actuators::ApagarLED( void ){
     digitalWrite(RELAY_PIN,HIGH);  //PIN RELAY
 }
 
 //Enciende el buzzer
-void class_actuators::EncenderBuzzer(void){
+void class_actuators::EncenderBuzzer( void ){
   EasyBuzzer.update();
   EasyBuzzer.beep(1000);
 }
 
 //Apaga el buzzer
-void class_actuators::ApagarBuzzer(void){
+void class_actuators::ApagarBuzzer( void ){
    EasyBuzzer.update();
    EasyBuzzer.stopBeep();
 }
