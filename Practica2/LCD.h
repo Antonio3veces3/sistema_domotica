9d2f6bccd7240c1c0f46aff0a0d554753c0473c8
zaholy16 <mmondragon@ucol.mx>
2022-05-15 20:12:29 -0500

    :tada: LCD.h finished

--- Practica2/LCD.h
+++ Practica2/LCD.h
@@ -1,6 +1,8 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CLASES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CLASE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 class class_lcd {
+    public:
+
     public:   //Métodos Públicos 
         void initLCD( void );          
         void printLCD( int, int, int ); 
@@ -29,24 +31,24 @@ void class_lcd::initLCD( void ){
 
 //Muestra los datos obtenidos en la LCD
 void class_lcd::printLCD( int temp, int hum, int lum ){  
+ 
+    RTC.formatDate(); //Obtiene la fecha en el formato dia/mes/año
+    RTC.formatTime();  //Obtiene la hora en el formato hora:minuto
 
-    RTC.formatDate();
-    RTC.formatTime();
-
-    String date_time = RTC.fecha + " " + RTC.hora;
+    String date_time = RTC.fecha + " " + RTC.hora;  //Guarda la fecha y hora en un string
 
     lcd.clear();
     lcd.createChar(0, customGrade); //Crea el icono personalizado
     lcd.setCursor(0,0); //Coloca el cursor en la posición dada
-    lcd.print(date_time);
+    lcd.print(date_time);  //Imprime la fecha 
     lcd.setCursor(0,1); 
     lcd.print(String(temp)); //Imprime la temperatura 
     lcd.setCursor(2,1); 
     lcd.write(0); //Escribe un espacio
     lcd.setCursor(3,1);  
-    lcd.print("C");  //Imprime la letra "C"
+    lcd.print("C");  //Imprime la letra "C" para la temperatura
     lcd.setCursor(5,1); 
     lcd.print("H:" + String(hum)+"%");  //Imprime la humedad
     lcd.setCursor(11,1);
-    lcd.print("L:" + String(lum));
+    lcd.print("L:" + String(lum)); //Imprime la luminosidad
 }
